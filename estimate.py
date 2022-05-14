e1c126fcad18a8704bcb8123a3238e480afbfde2
Yaechan Hong <yahong@c4r1s7.42seoul.kr>
2022-05-14 17:11:51 +0900

    feat: train

---
+++ estimate.py
@@ -0,0 +1,8 @@
+def estimatePrice(t0, t1, mileage):
+    return t0 + t1 * mileage
+
+def main():
+    print('main')
+
+if __name__ == '__main__':
+    main()
--- train.py
+++ train.py
@@ -1,7 +1,30 @@
-def 
+import csv
 
+def estimatePrice(t0, t1, mileage):
+    return t0 + t1 * mileage
+
+def train(t0, t1, learningRate, mileages, prices):
+    size = len(mileages)
+    s0, s1 = [0.0, 0.0]
+    for i in range(0, size):
+       s0 = (estimatePrice(t0, t1, mileages[i]) - prices[i])
+       s1 = (estimatePrice(t0, t1, mileages[i]) - prices[i]) * mileages[i]
+       t0 += learningRate * s0 / size
+       t1 += learningRate * s1 / size
+    return t0, t1
+ 
 def main():
-    print("test")
+    t0, t1, learningRate = [0.0, 0.0, 0.5]
+    mileages = []
+    prices = []
+    with open('data.csv', 'r', encoding='utf-8') as file:
+        csvReader = csv.reader(file)
+        for row in csvReader:
+            if (row[0] != 'km'):
+                mileages.append(float(row[0]))
+                prices.append(float(row[1]))
+    t0, t1 = train(t0, t1, learningRate, mileages, prices)
+    print(t0, t1)
 
 if __name__ == "__main__":
     main()
