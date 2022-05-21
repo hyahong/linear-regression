78a7e38c2b41bc377d8a3d4bf4adaaca6a59e8aa
yahong <yahong@student.42seoul.kr>
2022-05-21 11:57:58 +0900

    feat: train.py

--- train.py
+++ train.py
@@ -3,18 +3,36 @@ import csv
 def estimatePrice(t0, t1, mileage):
     return t0 + t1 * mileage
 
-def train(t0, t1, learningRate, mileages, prices):
-    size = len(mileages)
+def lossFunction(t0, t1, mileages, prices):
+    loss = 0.0
+    for mileage, price in zip(mileages, prices):
+        loss += (price - estimatePrice(t0, t1, mileage)) ** 2
+    return loss / len(mileages)
+
+def gradientDescent(t0, t1, learningRate, loss, mileages, prices):
     s0, s1 = [0.0, 0.0]
-    for i in range(0, size):
-       s0 = (estimatePrice(t0, t1, mileages[i]) - prices[i])
-       s1 = (estimatePrice(t0, t1, mileages[i]) - prices[i]) * mileages[i]
-       t0 += learningRate * s0 / size
-       t1 += learningRate * s1 / size
+    for mileage, price in zip(mileages, prices):
+        s0 += estimatePrice(t0, t1, mileage) - price
+        s1 += (estimatePrice(t0, t1, mileage) - price) * mileage
+    if loss > lossFunction(t0 - learningRate * s0 / len(mileages), t1 - learningRate * s1 / len(mileages), mileages, prices):
+        t0 -= learningRate * s0 / len(mileages)
+        t1 -= learningRate * s1 / len(mileages)   
+        loss = lossFunction(t0, t1, mileages, prices)
+        learningRate *= 1.04
+    else:
+        learningRate *= 0.6
+    return t0, t1, learningRate, loss
+
+def train(learningRate, epoch, mileages, prices):
+    t0, t1 = [0.0, 0.0]
+    loss = lossFunction(t0, t1, mileages, prices)
+    for _ in range(0, epoch):
+        t0, t1, learningRate, loss = gradientDescent(t0, t1, learningRate, loss, mileages, prices) 
+        print(t0, t1, learningRate)
     return t0, t1
  
 def main():
-    t0, t1, learningRate = [0.0, 0.0, 0.5]
+    learningRate, epoch = [0.4, 1000]
     mileages = []
     prices = []
     with open('data.csv', 'r', encoding='utf-8') as file:
@@ -23,8 +41,9 @@ def main():
             if (row[0] != 'km'):
                 mileages.append(float(row[0]))
                 prices.append(float(row[1]))
-    t0, t1 = train(t0, t1, learningRate, mileages, prices)
+    t0, t1 = train(learningRate, epoch, mileages, prices)
     print(t0, t1)
+    print(estimatePrice(t0, t1, 84000.0))
 
 if __name__ == "__main__":
     main()
